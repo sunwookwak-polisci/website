3f633bb76237db1e8535db158c23ea75947b920b
sunwookwak-polisci <sunwookwak@gmail.com>
2026-05-18 17:32:31 +0900

    fixed update

--- _source/render_cv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Render CV and move to docs folder
-
-cd "$(dirname "$0")"
-quarto render cv_source.qmd
-mv cv_source.pdf ../docs/cv_source.pdf
-echo "✓ CV rendered and moved to docs/cv_source.pdf"
